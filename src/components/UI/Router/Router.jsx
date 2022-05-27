5d3112ee0c3172128b2c26e6097b2ac1e1316606
Алексей Баранов <alex022003@mail.ru>
2022-05-27 19:21:37 +0300

    fixed unworkable router

--- src/components/UI/Router/Router.jsx
+++ src/components/UI/Router/Router.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { nanoid } from 'nanoid';
-import { RouteWays } from '../../../data/RouteWays';
+import CalendarPage from '../../../pages/CalendarPage/CalendarPage';
+import ErrorPage from '../../../pages/ErrorPage/ErrorPage';
+import HomePage from '../../../pages/HomePage/HomePage';
+import LoginPage from '../../../pages/LoginPage/LoginPage';
+import SignUpPage from '../../../pages/SignUpPage/SignUpPage';
+import TodoPage from '../../../pages/TodayTodoPage/TodoPage';
+
 const Router = () => {
     const id = nanoid;
     return (
         <Switch>
-            {RouteWays.map((item)=><Route key={id()} path={item.path}>{item.elem}</Route>)}
+            <Route key={id()} path='/login'>
+                <LoginPage/>
+            </Route>
+            <Route key={id()} path='/signup'>
+                <SignUpPage/>
+            </Route>
+            <Route key={id()} path='/error'>
+                <ErrorPage/>
+            </Route>
+            <Route key={id()} path='/home'>
+                <HomePage/>
+            </Route>
+            <Route key={id()} path='/today'>
+                <TodoPage/>
+            </Route>
+            <Route key={id()} path='/calendarik'>
+                <CalendarPage/>
+            </Route>
             <Redirect key={id()} to='/home'></Redirect>
         </Switch>
     );
--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Calendar from '../../components/UI/Calendar/Calendar';
 const CalendarPage= () => {
     return (
-        <><Calendar/></>
+        <>
+            <Calendar/>
+        </>
     );
 }
 
