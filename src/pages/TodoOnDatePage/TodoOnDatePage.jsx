95cdf8c907ef8cfa5383f067dcf0a699fc0fe3a8
Алексей Баранов <alex022003@mail.ru>
2022-06-02 23:22:28 +0300

    Preparing before adding todo functional

--- src/pages/TodoOnDatePage/TodoOnDatePage.jsx
+++ src/pages/TodoOnDatePage/TodoOnDatePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom';
 import getDateFromDotFormat from '../../scripts/getDateFromDotFormat';
 import { englishMonths } from '../../constants/EnglishMonths';
@@ -7,13 +7,15 @@ import classes from './TodoOnDatePage.module.css';
 import MyInput from '../../components/UI/LabelInput/MyInput';
 import { todayTodoArr } from '../../data/todayToDo';
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 const TodoOnDatePage = () => {
     //month is extended for client comfort
     const id = nanoid;
-    const todoArr = todayTodoArr.map((item, index)=>{
+    const [arr, setArr] = useState(todayTodoArr);
+    const [todoArr, setTodoArr] =useState (arr.map((item, index)=>{
         return <ToDo key={id()}  time = {item.time} text = {item.body}/>
-    })
+    }))
     const router = useHistory();
     const neededDate = getDateFromDotFormat( router.location.pathname.split('/')[2]);
     return (
