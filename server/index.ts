b54302a70cee17b691f45217657f48f42d97a2a7
1mpuser <alex022003@mail.ru>
2022-07-30 22:50:19 +0300

    Work with server started

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+require('dotenv').config();
+import express, { Application } from 'express'
+import { Sequelize } from 'sequelize';
+const sequelize: Sequelize = require('./db');
+const PORT : number = Number(process.env.PORT) || 7800;
+
+const app: Application = express();
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server started on port : ${PORT}`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
---
+++ server/models/models.ts
@@ -0,0 +1,63 @@
+import { DataTypes } from "sequelize/types";
+import { Sequelize } from 'sequelize';
+const sequelize : Sequelize = require('./db')
+
+const User = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement : true
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique : true
+    },
+    username: {
+        type: DataTypes.STRING,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+    }
+})
+const DateToDoCluster = sequelize.define('date_todo_cluster', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    date: {
+        type : DataTypes.DATEONLY
+    }
+});
+const ToDo = sequelize.define('todo', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement : true
+    },
+    time: {
+        type: DataTypes.TIME,
+    },
+    completed: {
+        type : DataTypes.BOOLEAN,
+    },
+    memo: {
+        type : DataTypes.STRING
+    }
+})
+
+
+
+
+User.hasMany(DateToDoCluster)
+DateToDoCluster.belongsTo(User);
+
+DateToDoCluster.hasMany(ToDo);
+ToDo.belongsTo(DateToDoCluster);
+
+module.exports = {
+    ToDo,
+    DateToDoCluster,
+    User
+}
